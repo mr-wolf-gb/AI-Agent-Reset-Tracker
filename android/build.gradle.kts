5c7b69c00af792557356bcd832274380169c0a4f
ʍᴙ.ώoŁƒ <4011175+mr-wolf-gb@users.noreply.github.com>
2026-06-16 12:03:22 +0100

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,10 +1,12 @@
 allprojects {
     repositories {
         google()
+        html()
         mavenCentral()
     }
 }
 
+// Custom build directory path configuration
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
@@ -15,13 +17,29 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+// Global configuration override for app and external plugins
 subprojects {
-    project.evaluationDependsOn(":app")
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        compilerOptions {
-            jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    afterEvaluate {
+        // Look for Android apps or library modules (like plugins) and force Java 17
+        if (plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")) {
+            extensions.configure<com.android.build.gradle.BaseExtension> {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
         }
     }
+
+    // Force Kotlin compiler tasks to target JVM 17
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
+
+    // Force standalone Java compiler tasks to target Java 17
     tasks.withType<JavaCompile>().configureEach {
         sourceCompatibility = "17"
         targetCompatibility = "17"
